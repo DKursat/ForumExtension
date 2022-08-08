a91bc7866cc10675d2fb3fcfa77203281aaf6079
Kursat <75574588+DKursat@users.noreply.github.com>
2022-08-08 22:25:20 +0200

    Summary

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projekt_website_visual_studio_community_Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+          
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int i = 0;
+            switch (i)
+            {
+                default:
+                    break;
+            }
+        }
+    }
+}
